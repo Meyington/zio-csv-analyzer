ef2b08dc47276030c8b5df086e80c623915fc164
st117171 <st117171@student.spbu.ru>
2025-05-13 13:29:32 +0300

    test: improve CSV parser test cases and naming

--- core/src/test/scala/com/github/meyington/csv/parser/CsvParserImplSpec.scala
+++ core/src/test/scala/com/github/meyington/csv/parser/CsvParserImplSpec.scala
@@ -7,42 +7,42 @@ import zio.test.Assertion.*
 object CsvParserImplSpec extends ZIOSpecDefault {
   def spec = suite("CsvParserImpl")(
     suite("ParseLine с настройками по умолчанию")(
-      test("Парсинг простой строки без специальных символов") {
+      test("Простая строка без специальных символов") {
         for {
           parser <- ZIO.succeed(CsvParserImpl())
           result <- parser.parseLine("a,b,c")
         } yield assert(result)(equalTo(CsvRow(Vector("a", "b", "c"))))
       },
 
-      test("Парсинг строки с пустыми значениями") {
+      test("Строка с пустыми значениями") {
         for {
           parser <- ZIO.succeed(CsvParserImpl())
           result <- parser.parseLine("a,,c")
         } yield assert(result)(equalTo(CsvRow(Vector("a", "", "c"))))
       },
 
-      test("Парсинг значений в кавычках") {
+      test("Значения в кавычках") {
         for {
           parser <- ZIO.succeed(CsvParserImpl())
           result <- parser.parseLine("""one,"two,with,commas",three""")
         } yield assert(result)(equalTo(CsvRow(Vector("one", "two,with,commas", "three"))))
       },
 
-      test("Парсинг экранированных символов") {
+      test("Экранированные символы") {
         for {
           parser <- ZIO.succeed(CsvParserImpl())
           result <- parser.parseLine("""normal,with\"quote,with\,comma""")
         } yield assert(result)(equalTo(CsvRow(Vector("normal", "with\"quote", "with,comma"))))
       },
 
-      test("Обработка ошибки при незакрытых кавычках") {
+      test("Ошибка при незакрытых кавычках") {
         for {
           parser <- ZIO.succeed(CsvParserImpl())
           result <- parser.parseLine("""one,"unclosed""").exit
         } yield assert(result)(fails(isSubtype[CsvError.ParseError](anything)))
       },
 
-      test("Обработка ошибки при неполном экранировании") {
+      test("Ошибка при неполном экранировании") {
         for {
           parser <- ZIO.succeed(CsvParserImpl())
           result <- parser.parseLine("""one,incomplete\""").exit
@@ -51,7 +51,7 @@ object CsvParserImplSpec extends ZIOSpecDefault {
     ),
 
     suite("ParseLine с пользовательской конфигурацией")(
-      test("Парсинг с использованием точки с запятой как разделителя") {
+      test("Использование точки с запятой как разделителя") {
         val config = new CsvConfig {
           val delimiter: Char = ';'
           val quoteChar: Char = '"'
@@ -65,7 +65,7 @@ object CsvParserImplSpec extends ZIOSpecDefault {
     ),
 
     suite("ParseLines")(
-      test("Парсинг нескольких строк без специальных символов") {
+      test("Несколько строк без специальных символов") {
         for {
           parser <- ZIO.succeed(CsvParserImpl())
           result <- parser.parseLines(Seq("a,b", "c,d", "e,f"))
@@ -76,7 +76,7 @@ object CsvParserImplSpec extends ZIOSpecDefault {
         )))
       },
 
-      test("Обработка прерывания при ошибке парсинга") {
+      test("Прерывание при ошибке парсинга") {
         for {
           parser <- ZIO.succeed(CsvParserImpl())
           result <- parser.parseLines(Seq("a,b", """c,"unclosed""", "e,f")).exit
